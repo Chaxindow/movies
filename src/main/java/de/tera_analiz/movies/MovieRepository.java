9e59085da1f3fa004f68a99410f129e5601c73d0
Chaxindow <78871407+Chaxindow@users.noreply.github.com>
2023-11-02 13:21:25 +0300

    first commit

---
+++ src/main/java/de/tera_analiz/movies/MovieRepository.java
@@ -0,0 +1,15 @@
+package de.tera_analiz.movies;
+
+import com.mongodb.client.MongoDatabase;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    Optional<Movie> findMovieByImdbId (String imdbId);
+
+}
